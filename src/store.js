a20e7ab14d69d35d077f02387ba6f108d294e613
doeunllee <80470153+doeunllee@users.noreply.github.com>
2021-05-03 19:00:17 +0900

    back-front

--- src/store.js
+++ src/store.js
@@ -1,6 +1,25 @@
 import { createStore } from "redux";
 import _data from "data.json";
+import axios from "axios/axios";
 
+const getQuestion=()=>{
+  const questions=[];
+  axios.get("question/all").then((result)=>{
+    result.data.forEach((item)=>{
+      questions.push(item);
+  });
+});
+  return questions;
+};
+const getAnswer=()=>{
+  const answers=[];
+  axios.get("answer/all").then((result)=>{
+    result.data.forEach((item)=>{
+      answers.push(item);
+  });
+});
+  return answers;
+};
 function reducer(state, action) {
   if (state === undefined) {
     let mockData = _data.concat();
@@ -8,15 +27,23 @@ function reducer(state, action) {
       name: "",
       selected_answer: 0,
       data: mockData,
+      question:getQuestion(),
+      answer:getAnswer(),
+      answer1:[],
+      answer2:[]
     };
   }
-
+  const answer = [...state.answer];
+  
+  const answer1=answer.filter((answer)=> answer.answerId = 1)
+  const answer2=answer.filter((answer)=> answer.answerId =2)
   switch (action.type) {
     case "setName":
       return { ...state, name: action.name };
     default:
       break;
   }
+  return { ...state, answer1:answer1,answer2:answer2 };
 }
 export default createStore(
   reducer,
